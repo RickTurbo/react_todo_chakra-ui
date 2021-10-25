c045af985ea91d2d03bcda4975087c314b0c0c2b
kojima rikuto <kojikoji7765@icloud.com>
2021-10-26 00:01:51 +0900

    AddTodo

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,11 +1,20 @@
+import { Button } from '@chakra-ui/button'
+import { Input } from '@chakra-ui/input'
+import { HStack } from '@chakra-ui/layout'
 import React from 'react'
 
 function AddTodo() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+function handleSubmit(e) {
+
+}
+
+  return <form onSubmit={handleSubmit}>
+    <HStack mt='8'>
+      <Input variant="filled" placeholder='learning chakraui with todo app'/>
+      <Button colorScheme="pink" px='8' type='submit'>Add Todo</Button>
+    </HStack>
+  </form>
 }
 
 export default AddTodo
